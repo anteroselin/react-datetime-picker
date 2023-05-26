a02972d82941b6728ab152c41b24208c28deaf7f
anteroselin <anteroselin@gmail.com>
2023-05-27 02:16:41 +0800

    Add month test
    Co-authored-by: arthur9371 <130764940+arthur9371@users.noreply.github.com>

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,11 +9,21 @@ import "./header.scss";
 const Header: FC<HeaderProps> = ({ date, nextDisabled, prevDisabled, onClickNext, onClickPrevious }) => {
   return (
     <div className="header-container">
-      <button className="header-icon prev" disabled={prevDisabled} onClick={onClickPrevious} />
+      <button
+        className="header-icon prev"
+        disabled={prevDisabled}
+        onClick={onClickPrevious}
+        data-testid="test-prev-month"
+      />
       <span className="header-text">
         {moment(date).format("MMMM")} {date.getFullYear()}
       </span>
-      <button className="header-icon next" disabled={nextDisabled} onClick={onClickNext} />
+      <button
+        className="header-icon next"
+        disabled={nextDisabled}
+        onClick={onClickNext}
+        data-testid="test-next-month"
+      />
     </div>
   );
 };
